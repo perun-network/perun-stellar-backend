6ed4799a79176ee167ad55779f957dd12328d207
Ilja von Hoessle <ilja.bvh@protonmail.com>
2024-01-04 16:15:04 +0100

    Add license, remove comments

--- channel/funder_test.go
+++ channel/funder_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,26 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package test
-
-import (
-	"context"
-	pchannel "perun.network/go-perun/channel"
-	"perun.network/perun-stellar-backend/channel"
-	pkgerrors "polycry.pt/poly-go/errors"
-)
-
-func FundAll(ctx context.Context, funders []*channel.Funder, reqs []*pchannel.FundingReq) error {
-	g := pkgerrors.NewGatherer()
-	for i := range funders {
-		i := i
-		g.Go(func() error {
-			return funders[i].Fund(ctx, *reqs[i])
-		})
-	}
-
-	if g.WaitDoneOrFailedCtx(ctx) {
-		return ctx.Err()
-	}
-	return g.Err()
-}
+package channel_test
--- channel/test/setup.go
+++ channel/test/setup.go
@@ -1,16 +1,26 @@
+// Copyright 2023 PolyCrypt GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package test
 
 import (
 	"math/big"
-	"testing"
-
-	//"perun.network/go-perun/backend/sim/wallet"
 	pchannel "perun.network/go-perun/channel"
-	pwallet "perun.network/go-perun/wallet"
-	//pwtest "perun.network/go-perun/wallet/test"
-
 	ptest "perun.network/go-perun/channel/test"
+	pwallet "perun.network/go-perun/wallet"
 	pkgtest "polycry.pt/poly-go/test"
+	"testing"
 )
 
 func NewParamsState(t *testing.T) (*pchannel.Params, *pchannel.State) {
